5bf317ab17c9203434cfc167078c928fd18bbc02
Chris Martinez <smartinez79@protonmail.com>
2019-06-19 09:11:03 -0500

    update to htmlRoutes.js file

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,12 @@
+var path = require("path");
+    // Routes
+module.exports = function (app) {
+    // Home Page
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+    // Survey Page
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+}
