6647643263b9648007748ef1ca89fffc61338625
Chris Martinez <smartinez79@protonmail.com>
2019-06-19 04:50:23 -0500

    update to server.js file

---
+++ server.js
@@ -0,0 +1,12 @@
+//Dependencies
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//Router
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
