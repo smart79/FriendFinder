f3317171b0d087030e47c2edb3033c55852845e5
Chris Martinez <smartinez79@protonmail.com>
2019-06-19 14:36:12 -0500

    update to survey

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,12 @@
-var friends = require("../data/friends");
+var friends = require("../data/friends.js");
 
 module.exports = function(app) {
 
-    app.get('/api/friends', function(req, res) {
+    app.get("/api/friends", function(req, res) {
         return res.json(friends);
     });
 
-    app.post('/api/friends', function(req, res) {
+    app.post("/api/friends", function(req, res) {
 
         // finds smallest number in array 
         Array.min = function( array ){
