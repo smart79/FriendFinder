842b88ffb5737f059c7fb839097655ce2c8d53fc
Chris Martinez <smartinez79@protonmail.com>
2019-06-21 22:55:23 -0500

    Update to the routes folder

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,12 +1,12 @@
 var path = require("path");
-    // Routes
+// Routes
 module.exports = function (app) {
     // Home Page
-    app.get("/", function(req, res) {
+    app.get("/home", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
     // Survey Page
-    app.get("/survey", function(req, res) {
+    app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 }
--- server.js
+++ server.js
@@ -7,12 +7,12 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static("app/public"));
 //Router
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 
 // Starts the server to begin listening
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT: " + PORT);
 });
