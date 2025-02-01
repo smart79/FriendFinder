adf322c26435e17044c05e305d1d9b944c6792fa
SMartinez <46066859+smart79@users.noreply.github.com>
2025-01-31 21:04:21 -0600

    routing

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve static files (this is what allows images to load)
-app.use(express.static(__dirname + "/public"));
+app.use('/images',express.static(__dirname + "/public"));
 
 //Router
 require("./app/routing/apiRoutes.js")(app);
